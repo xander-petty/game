69ec40c54fb39f6af601cb6ca0de798647fd5b78
Xander <xander.petty@icloud.com>
2024-01-21 16:05:24 -0600

    Created test units

---
+++ test_game.py
@@ -0,0 +1,26 @@
+import unittest
+import pygame
+import game
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        self.game = game
+
+    def test_initialization(self):
+        self.assertEqual(self.game.TITLE, 'Practice Project')
+        self.assertEqual(self.game.RESOLUTION, (800, 600))
+        self.assertEqual(self.game.BLACK, (0, 0, 0))
+        self.assertEqual(self.game.WHITE, (255, 255, 255))
+
+    def test_game_loop(self):
+        self.game.running = True
+        self.game.pygame.event.post(pygame.event.Event(pygame.QUIT))
+        self.game.main()  # This should run the game loop until pygame.QUIT is posted
+        self.assertFalse(self.game.running)
+
+    def tearDown(self):
+        pygame.quit()
+
+if __name__ == '__main__':
+    unittest.main()
